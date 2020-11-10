e2ddc0236416433d242772a7c20a6076a59057c6
Uday Kumar <kumarnzu@amazon.com>
2020-11-10 16:55:43 +0530

    ut structure

---
+++ unit-tests/nudfs/nud-common/common.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_SUITE( nud_common_ut )
+
+BOOST_AUTO_TEST_CASE( nud_common_ut_simple )
+{
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
