f0bc1652e9b6af007003e9748b05173f42906494
Uday Kumar <kumarnzu@amazon.com>
2020-11-11 22:28:12 +0530

    experimental things

--- nudfs/nud-dev/dev.hxx
+++ nudfs/nud-dev/dev.hxx
@@ -7,13 +7,16 @@ namespace nudfs
 {
     namespace storage
     {
+
         struct device
         {
             explicit device(const uint8_t id = 0);
-            ~device() { }
+            ~device() {}
 
             uint8_t id_;
         };
+
+
     } // namespace storage
 } // namespace nudfs
 
